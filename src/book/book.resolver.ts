e0bf11b1d46b27fab928a1891947bea61915bc26
dotundenton <dotundenton@gmail.com>
2023-11-05 02:19:50 +0100

    Added Update and Delete methods to both book services and resolver

--- src/book/book.resolver.ts
+++ src/book/book.resolver.ts
@@ -2,26 +2,36 @@ import { Inject } from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './book.model';
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
-import { v4 as uuid } from 'uuid'
+import { v4 as uuid } from 'uuid';
+import { BookDto } from './book.dto';
 
 @Resolver(of => Book)
-export class BookResolver{
+export class BookResolver {
   constructor(@Inject(BookService) private bookService: BookService) {
   }
 
-@Query(returns => Book)
-  async book(@Args('id') id: string): Promise<Book>{
+  @Query(returns => Book)
+  async book(@Args('id') id: string): Promise<Book> {
     return await this.bookService.findOne(id);
-}
+  }
 
-@Query(returns => [Book])
-  async books():Promise<Book[]>{
+  @Query(returns => [Book])
+  async books(): Promise<Book[]> {
     return await this.bookService.findAll();
-}
+  }
 
+  @Mutation(returns => Book)
+  async createBook(@Args('name') name: string, @Args('description') description: string): Promise<Book> {
+    return await this.bookService.createBook({ name, description });
+  }
 
-@Mutation(returns => Book)
-  async createBook(@Args('name') name: string, @Args('description') description: string): Promise<Book>{
-    return await this.bookService.createBook({ name, description })
-}
+  @Mutation(returns => Book)
+  async updateBook(@Args('id') id: string, @Args('name') name: string, @Args('description') description: string): Promise<Book> {
+    return await this.bookService.updateBook(id, { name, description });
+  }
+
+  @Mutation(returns => String)
+  async deleteBook(@Args('id') id: string): Promise<String> {
+    return await this.bookService.deleteBook(id);
+  }
 }
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Book } from './book.model';
 import { Repository } from 'typeorm';
@@ -8,13 +8,35 @@ import { BookDto } from './book.dto';
 export class BookService {
   constructor(@InjectRepository(Book) private bookRepository: Repository<Book>) {
   }
-  createBook(book: BookDto): Promise<Book>{
+
+  createBook(book: BookDto): Promise<Book> {
     return this.bookRepository.save(book);
   }
-  findAll(): Promise<Book[]>{
+
+  findAll(): Promise<Book[]> {
     return this.bookRepository.find();
   }
+
   findOne(id: string): Promise<Book> {
-    return this.bookRepository.findOneBy({id: id});
+    return this.bookRepository.findOneBy({ id: id });
+  }
+
+  updateBook(id: string, updateBookDto: BookDto): Promise<Book> {
+    const book = this.findOne(id);
+    if (!book) {
+      throw new Error('Book not found');
+    }
+    Object.assign(book, updateBookDto);
+    try {
+      const updatedBook = this.bookRepository.save({ id: id, book });
+      return updatedBook;
+    } catch (error) {
+      throw new Error('Error updating the book');
+    }
+  }
+
+  deleteBook(id: string) {
+    const result = this.bookRepository.delete(id);
+    return { message: 'Book successfully deleted' };
   }
 }
